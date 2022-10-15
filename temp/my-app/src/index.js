58a23df41431339d7a067f001e581b3b865573b9
omarshivji <omar.shivji@gmail.com>
2022-10-15 13:06:16 +0000

    tutorial button added to import app

--- temp/my-app/src/index.js
+++ temp/my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './tutorial_button';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
