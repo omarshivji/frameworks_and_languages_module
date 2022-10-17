09c08388ead069ea1c9fef3bd0d66ba693655cd0
omarshivji <omar.shivji@gmail.com>
2022-10-17 09:27:39 +0000

    testing with pytests

---
+++ temp/pytest_example/test_example.py
@@ -0,0 +1,19 @@
+from example import *
+
+
+def test_add():
+      assert add(1, 2) == 3
+      assert add(1, -2) == -1
+      assert add(1000000000000000000, 2) == 1000000000000000002
+
+def test_multiply():
+      assert multiply(2, 2) == 4
+      assert multiply(3, 7) == 21
+
+def test_divide():
+    assert divide(21, 7) == 3
+    assert divide(90, 10) == 9
+
+def test_minus():
+    assert minus(21, 10) == 11
+    assert minus(55, 5) == 50
