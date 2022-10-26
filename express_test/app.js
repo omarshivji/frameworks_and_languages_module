341b731e42b1d132cc8dca3d3d653912012765b0
omarshivji <omar.shivji@gmail.com>
2022-10-26 17:50:41 +0000

    2nd test pass

--- express_test/app.js
+++ express_test/app.js
@@ -2,13 +2,29 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.use(express.json());
+
+ATTENDEES = []
+
 app.get('/', (req, res) => {
   res.send('<html><body>Your HTML text</body></html>')
 })
 
-app.post('/attendee/', (req, res) => {
+app.post('/attendee', (req, res) => {
+  console.log('I got a post')
+  console.log('I got res.body', req.body)
   ATTENDEES.push(req.body)
   res.status(201).json(req.body)
+})
+
+app.get('/attendees', (req, res) => {
+  res.json(ATTENDEES)
+})
+
+app.delete('/attendee/:id', (req, res) => {
+  //console.log('Delete!', req.params.id, 123)
+  ATTENDEES = ATTENDEES.filter(o => o.id !== parseFloat(req.params.id))
+  res.status(204).json()
 })
 
 app.listen(port, () => {
