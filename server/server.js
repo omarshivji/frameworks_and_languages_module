022305d1e9fdb970dba68b139cf86a6b49a4587f
omarshivji <omar.shivji@gmail.com>
2022-10-27 17:13:31 +0000

    server port open and html on

--- server/server.js
+++ server/server.js
@@ -1,3 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 8000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+  })
+
+  app.get('/item', (req, res) => {
+   res.json('<html><body>Your Items</body></html>')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
+  process.on('SIGINT', function() {process.exit()})
