459b098e78126c78450029a96b7df55d723dcd96
omarshivji <omar.shivji@gmail.com>
2022-10-28 18:58:58 +0000

    curl commands

--- server/server.js
+++ server/server.js
@@ -9,15 +9,14 @@ ITEMS = []
 app.get('/', (req, res) => {
     res.send('<html><body>Your Items</body></html>')
   })
+ 
+app.post('/item', (req, res) => {
+  ITEMS.push(req.body)
+  res.status(201).json(req.body)
+})
 
-
-  app.post('/item', (req, res) => {
-    ITEMS.push(req.body)
-    res.status(201).json(req.body)
-  })
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
   
-  process.on('SIGINT', function() {process.exit()})
+process.on('SIGINT', function() {process.exit()})
