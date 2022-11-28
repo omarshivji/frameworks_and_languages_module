00b0a1ca45a4d330261e8d47186ee40304ffb078
omarshivji <omar.shivji@gmail.com>
2022-11-28 19:06:10 +0000

    code cleanup in server

--- server/server.js
+++ server/server.js
@@ -30,16 +30,6 @@ var ITEMS = {
 }
 
 // POST
-/* app.post('/item', (req, res) => {
-  if(isNaN(req.body.id)){
-    res.status(405).json(req.body)
-  }else{
-    ITEMS.push(req.body)
-    res.status(201).json(req.body)
-  }
-})
-*/
-// POST
 // https://medium.com/@anshurajlive/read-dictionary-data-or-convert-dictionary-into-an-array-of-objects-in-javascript-e9c52286d746
 app.post('/item', (req, res) => {
   const itemID = Object.keys(ITEMS).length +1 
@@ -62,38 +52,19 @@ app.post('/item', (req, res) => {
   }
   else {
     res.status(405).json('Missing field')
-    // console.log('Missing field')
   }
 })
   
 
 // GET
-
-/*
-app.get('/items', (req, res)=> {
-  const uITEMS = ITEMS.filter(o => o.id !== req.params.id)
-  if (parseInt(Object.keys(ITEMS)).length>0) 
-  {
-    res.status(200).json(Object.values(ITEMS))
-  }
-  else{
-    res.status(404).json("This user cannot be found.")
-  }
-})
-*/
-
-
 app.get('/', (req, res) => {
     res.send('<html><body>Your Items</body></html>')
   })
 
- 
-
 app.get('/item/:id', (req,res) => {
-  //if(Object.keys(ITEMS).includes(req.query.user_id)){
+
   if(ITEMS[req.params.id] === undefined){ // https://stackoverflow.com/questions/39045367/how-to-delete-on-express-without-specificate-the-whole-route (helped me figure out that if it displays undefined, then show 404 status)
     res.status(404).json('This item does not exist')
-    // console.log('This item does not exist')
   } else{
     res.status(200).json(ITEMS[req.params.id])
   }
@@ -112,34 +83,18 @@ app.get('/items', (req, res) => {
     res.send(ITEMS)
 })
   
-
-
 //DELETE
-
-/* 
-app.delete('/items/:id', (req, res) => {
-  console.log('Deleted', req.params.id)
-  let uITEMS = ITEMS.filter(o => o.id !== req.params.id)
-    res.status(204).json()
-
-})
-*/
-
 app.delete('/item/:id', (req, res) => {
-  // console.log('Deleted', req.params.id)
   let idITEMS = parseInt(req.params.id) // Getting and turning the id into an integer 
   if(ITEMS.hasOwnProperty(idITEMS))
 {
   delete ITEMS[idITEMS]
   res.status(204).send('Deleted')
-  // console.log('Deleted')
 }  
 else {
   res.status(404).send('This item cannot be found')
- // console.log('This item cannot be found')
 }
 })
-
 
 //CORS 
 // https://expressjs.com/en/resources/middleware/cors.html
